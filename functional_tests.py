7a4b96159ad66e2296cb8127e94759e5c7c7122d
alener <alener014@gmail.com>
2014-12-31 10:51:46 -0300

    before you do any refactoring, always do a commit

--- functional_tests.py
+++ functional_tests.py
@@ -2,22 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  #1
+class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):  #2
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):  #3
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #5
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -39,12 +39,16 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         inputbox.send_keys(Keys.ENTER)
 
+        #import time
+        #time.sleep(10)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-            )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+             [row.text for row in rows]
+        )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
@@ -57,5 +61,5 @@ class NewVisitorTest(unittest.TestCase):  #1
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -18,6 +18,21 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+            {'new_item_text':  'A new list item'}
+            )
+
+        self.assertEqual(response.content.decode(), expected_html)
+
+        
 
         #self.assertTrue(response.content.strip().startswith(b'<html>'))  #3
         #self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
