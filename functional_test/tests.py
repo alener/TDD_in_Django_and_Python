813607453e0ae52cdde4c89aea5696213c9c1cc5
alener <alener014@gmail.com>
2015-01-02 10:10:57 -0300

    correcting a bug in class NewVisitorTest

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -51,7 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        #self.fail('Finish the test!')
+
+        
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -59,9 +60,7 @@ class NewVisitorTest(unittest.TestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-
+        self.fail('Finish the test!')
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
