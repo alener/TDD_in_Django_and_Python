e5590be71b8cb8657a0d7980dd1b02842077bc6f
alener <alener014@gmail.com>
2015-01-05 15:50:39 -0300

    Other Refactoring in FT:Adjusting our models

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def view_list(request):
     items = Item.objects.all()
@@ -10,7 +10,8 @@ def home_page(request):
     return render (request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 
